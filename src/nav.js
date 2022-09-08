cf3f078dea49c4d8259d7716e9b7795254ee1e40
mollycaffery10 <mollycaffery10@gmail.com>
2022-09-08 13:42:50 -0400

    WIP trying to add navigation functionality.

---
+++ src/nav.js
@@ -0,0 +1,16 @@
+import {Link} from "react-router-dom";
+
+
+function Nav() {
+    return(
+    <nav>
+        <h3>My Navbar</h3>
+            <ul>
+                <li>Home</li>
+                <li>Weather</li>
+            </ul>
+    </nav>
+    );
+}
+
+export default Nav;
---
+++ src/weatherApp.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Weather() {
+return (
+	<div>
+	<h1>
+		Weather app that is a WIP.
+	</h1>
+	</div>
+);
+};
+
+export default Weather;
