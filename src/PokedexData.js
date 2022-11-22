35a53e4e162161c6b1d64db8b4bbc801fb6df532
mollycaffery10 <mollycaffery10@gmail.com>
2022-11-22 11:47:21 -0500

    Added useEffect

--- src/PokedexData.js
+++ src/PokedexData.js
@@ -1,13 +1,18 @@
-import React, { useState} from "react";
+import React, { useState, useEffect } from "react";
 import PokemonList from './PokemonList'
 import axios from "axios";
 
 function App() {
     const [pokemon, setPokemon] = useState([])
+    const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon/")
 
-    axios.get("https://pokeapi.co/api/v2/pokemon/").then(res => {
-        setPokemon(res.data.results.map(p => p.name))
-    })
+    //Everytime we change current page URL then we re-run the code inside of here.
+    useEffect(() => {
+        axios.get(currentPageUrl).then(res => {
+            setPokemon(res.data.results.map(p => p.name))
+        })
+        }, [currentPageUrl])
+
     return (
         <PokemonList pokemon={pokemon} />
     )
